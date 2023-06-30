b222cb88ffb3eae7010efed8a2618825a7866cf4
Bcypher01 <olumidebabayemi1@gmail.com>
2023-06-30 09:58:18 +0100

    change all jsx files in components back to js

--- components/Pagination.js
+++ components/Pagination.js
@@ -5,7 +5,7 @@ const Pagination = () => {
         <svg
           width="19"
           height="19"
-          className="mt-2"
+          className="mt-2 cursor-pointer"
           viewBox="0 0 19 19"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -15,23 +15,23 @@ const Pagination = () => {
             <path d="M14 14L9.5 9.5L14 5" stroke="#262626" stroke-width="2" />
           </g>
         </svg>
-        <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
+        <span className="border border-neutral-400 cursor-pointer rounded-md text-center pt-1 w-[35px] h-[35px]">
           1
         </span>
-        <span className="border bg-blue-600 text-white border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
+        <span className="border bg-blue-600 text-white border-neutral-400 cursor-pointer rounded-md text-center pt-1 w-[35px] h-[35px]">
           2
         </span>
-        <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
+        <span className="border border-neutral-400 cursor-pointer rounded-md text-center pt-1 w-[35px] h-[35px]">
           3
         </span>
-        <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
+        <span className="border border-neutral-400 cursor-pointer rounded-md text-center pt-1 w-[35px] h-[35px]">
           4
         </span>
         <svg
           width="19"
           height="19"
           viewBox="0 0 19 19"
-          className="mt-2"
+          className="mt-2 cursor-pointer"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
