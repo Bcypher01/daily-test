080b64173d0c1eed0f5b3fc41714cc41d36a5af6
Bcypher01 <olumidebabayemi1@gmail.com>
2023-06-30 09:01:59 +0100

    refactor pagination

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,3 +1,4 @@
+import Pagination from "./Pagination";
 import { Statistics } from "./Statistics";
 import { Table } from "./Table";
 
@@ -30,58 +31,7 @@ export const Dashboard = () => {
 
         <Table />
         {/* Pagination */}
-        <div className="grid py-4 place-items-center">
-          <div className="flex space-x-2">
-            <svg
-              width="19"
-              height="19"
-              className="mt-2"
-              viewBox="0 0 19 19"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g opacity="0.4">
-                <path
-                  d="M9.5 14L5 9.5L9.5 5"
-                  stroke="#262626"
-                  stroke-width="2"
-                />
-                <path
-                  d="M14 14L9.5 9.5L14 5"
-                  stroke="#262626"
-                  stroke-width="2"
-                />
-              </g>
-            </svg>
-            <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
-              1
-            </span>
-            <span className="border bg-blue-600 text-white border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
-              2
-            </span>
-            <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
-              3
-            </span>
-            <span className="border border-neutral-400 rounded-md text-center pt-1 w-[35px] h-[35px]">
-              4
-            </span>
-            <svg
-              width="19"
-              height="19"
-              viewBox="0 0 19 19"
-              className="mt-2"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.5 14L14 9.5L9.5 5"
-                stroke="#262626"
-                stroke-width="2"
-              />
-              <path d="M5 14L9.5 9.5L5 5" stroke="#262626" stroke-width="2" />
-            </svg>
-          </div>
-        </div>
+        <Pagination />
       </div>
     </div>
   );
